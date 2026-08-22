88d531192b57065c451c9ecc5d88075b4ceec896
Marielly Leal Castro <lealmariellyc@gmail.com>
2026-08-21 23:55:51 -0300

    não aguento mais

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -42,10 +42,7 @@ export default function Hero() {
 
           <div className={styles.socialProof}>
             <div className={styles.avatars}>
-              <span>1</span>
-              <span>2</span>
-              <span>3</span>
-              <span>4</span>
+              <span></span>
             </div>
 
             <div className={styles.proofText}>
